6ee760e5b6ae7272da4ac980cdcceee9cd967aaf
Gladwin chhinchani <gladwinch20@gmail.com>
2020-11-23 04:22:22 +0530

    added bootcamp resource

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,34 +1,97 @@
+const ErrorResponse = require('../utils/errorResponse')
+const geocoder = require('../utils/geocoder')
+const asyncHandler = require('../middleware/async')
+const Bootcamp = require('../models/Bootcamp')
+
 //@desc       Get all bootcamps
 //@route      Get /api/v1/bootcamps
 //@access     Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps', middle: req.name})
-}
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    let query
+    let queryStr = JSON.stringify(req.query)
+    queryStr = queryStr.replace(/\b(gt|lt|gte|lte|in)\b/g, match => `$${match}`)
+    console.log("check query string: ", queryStr)
+
+    query = Bootcamp.find(JSON.parse(queryStr))
+    
+    const bootcamp = await query
+    res.status(200).json({ success: true, count: bootcamp.length, data: bootcamp })
+})
 
 //@desc       Get single bootcamp
 //@route      Get /api/v1/bootcamps/:id   
 //@access     Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show single bootcamp' })
-}
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id)
+
+    if(!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        )
+    }
+    res.status(200).json({ success: true, data: bootcamp }) 
+})
 
 //@desc       Create new bootcamp
 //@route      POST /api/v1/bootcamps
 //@access     Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamp' })
-}
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.create(req.body)
+    res.status(201).json({ success: true, data: bootcamp }) 
+})
 
 //@desc       Update bootcamp
 //@route      PUT /api/v1/bootcamps/:id
 //@access     Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Update bootcamp' })
-}
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    if (!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        )
+    }
+
+    res.status(200).json({ success: true, data: bootcamp })
+})
 
 //@desc       Delete bootcamp
 //@route      PUT /api/v1/bootcamps/:id
 //@access     Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'delete bootcamp' })
-} 
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+
+    if (!bootcamp) {
+        return next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        )
+    }
+
+    res.status(200).json({success: true, data: {} })
+})
+
+//@desc       Get bootcamps with a radius
+//@route      PUT /api/v1/bootcamps/radius/:zipcode/:distance
+//@access     Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params
+
+    //Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode)
+    const lat = loc[0].latitude
+    const lng = loc[0].longitude
+
+    //Calc radius using radians
+    const radius = distance / 3963
+
+    const bootcamps = await Bootcamp.find({
+        location: {
+            $geoWithin: { $centerSphere: [ [ lng, lat ], radius ] }
+        }
+    })
+
+    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
+})
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const colors = require('colors')
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db')
 
 //Load env vars
@@ -14,24 +16,25 @@ const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
+// Body parser
+app.use(express.json())
+
 //Dev logging middleware
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-// Body parser
-app.use(express.json());
-
-
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/bootcamps', bootcamps)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV.yellow} on port ${PORT.green}`))
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`)
+    console.log(`Error: ${err.message.red}`)
     server.close(() => process.exit(1))
 })
